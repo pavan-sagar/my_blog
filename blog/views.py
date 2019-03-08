824f47682d5cac5f58d9dc6fab9b1b30ea7914f1
unknown <pavan.sagar@outlook.com>
2019-03-08 19:16:34 +0530

    First commit : We have the home and about page with a sidebar and a working database serving sample data to posts at home

---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+
+posts = [
+
+{
+'author':'Pavan',
+'title':'First blog post',
+'date_posted':'06.03.2019',
+'content':'This is my post regarding the django blog'
+},
+{
+'author':'Nazim',
+'title':'Second blog post',
+'date_posted':'07.03.2019',
+'content':"I'm planning to go for my higher studies" 
+}
+]
+
+context={'posts':Post.objects.all(),'title': "Pavan's"}
+
+def home(request):
+    
+    return render(request,'blog/home.html',context)
+
+def about(request):
+    return render(request,'blog/about.html')
