b591b924152d83732ffbdb55d588cab47b1dccbb
unknown <pavan.sagar@outlook.com>
2019-03-11 20:58:56 +0530

    Added profile update feature and author picture for posts

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
@@ -7,3 +8,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return("{} Profile".format(self.user.username))
+
+#In-built save function is overridden to resize a large user profile image to the needed size which reduces page load time.
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     '''Here whenever we submit, the inputs are sent by a POST request but to the same page.
@@ -19,6 +19,22 @@ def register(request):
     # We pass data to the template using a dictionary with key as var name and value as the var
     return render(request,'users/register.html',{'form':form})
 
-@login_required
+@login_required #This decorator will redirect guest user to the login page if they try to access the /profile url.
 def profile(request):
-    return render(request,'users/profile.html')
+    
+    #If user has updated details and clicked the "Update" button, we make the required changes for the user DB tables. The request.POST and request.FILES is passing the newly entered data by user.
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request,'Your account has been updated.')
+            return redirect('profile') #The advantage of redirecting here instead of letting the if block complete is the browser doesnt ask for form confirmation and this also prevents any duplicate rows creation in DB.
+    else: #Else part means we are lading to this page using the 'Profile' link from banner and just pre-filling form with user details.
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {'u_form':u_form,'p_form':p_form}
+    return render(request,'users/profile.html',context)
